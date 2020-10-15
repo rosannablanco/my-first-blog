21219a3297313b57098308297032329f94bf1130
Rosanna <rosanna.blanco.prado@gmail.com>
2020-10-15 19:18:18 +0200

    He añadido template y archivo post_list html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+# def is a funtion of name post_list
+
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
